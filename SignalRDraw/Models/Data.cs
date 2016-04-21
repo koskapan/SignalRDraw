0acdcd20780c062060e6846b3f01a462ee779e14
koskapan <koskapan@gmail.com>
2016-04-21 16:38:01 +0300

    implementing data contract

---
+++ SignalRDraw/Models/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace SignalRDraw.Models
+{
+    public class Data
+    {
+        [JsonProperty("startX")]
+        public int StartX { get; set; }
+        [JsonProperty("startY")]
+        public int StartY { get; set; }
+        [JsonProperty("endX")]
+        public int EndX { get; set; }
+        [JsonProperty("endY")]
+        public int EndY { get; set; }
+    }
+}
